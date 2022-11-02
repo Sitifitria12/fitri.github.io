8f7b797a4d8bdbacf89f2c40f56a63285ee2fc82
Sitifitria12 <sittifitriyah3010@gmail.com>
2022-11-02 11:36:55 +0700

    fitri tugas percabangan

---
+++ index.js
@@ -0,0 +1,153 @@
+// console.log("HELLO FITRI");
+
+// var a = 12;
+
+// console.log(a);
+
+
+// oprator pada aritmatika ( * + - /)
+
+
+
+// var bil1 = 11;
+// var bil2 = 12;
+
+// var hasil =( bil1 + bil2);
+// var hasil1 = (bil1* bil2);
+// var hasil2 = (bil1 / bil2);
+// var hasil3 = (bil1 - bil2);
+
+// console.log(hasil);
+// console.log(hasil1);
+// console.log(hasil2);
+// console.log(hasil3);
+
+
+
+
+
+// var alas = 15;
+// var tinggi = 18;
+
+// var hasil = 1/2*(alas * tinggi);
+
+// console.log(hasil);
+
+// let a =14 == 14;
+
+// console.log(a);
+
+
+// let nilai = 32;
+
+// if(nilai >= 70){
+//     console.log("selamat kamu lulus");
+// } else{
+// console.log("kamu harus belajar lagi");}
+
+
+//  let nilai =102;
+
+//  if (nilai >=90 && nilai <= 100){
+//     console.log(nilai +" nilai sangat Memuaskan");
+//  } else if(nilai >=80 && nilai <= 89){
+//     console.log(nilai +" nilai memuaskan");
+//  }else if(nilai >= 70 && nilai <=79){
+//     console.log(nilai + " nilai cukup");
+//  }else{
+//     console.log(nilai + " nilai kurang");
+//  }
+
+
+// const email = "sittifitriyah3010@gmail.com";
+// const password = "12345678";
+
+// if (email === "sittifitriyah3010@gmail.com"){
+//     if (password === "12345678"){
+//      console.log("kamu berhasil login");
+//     }else{
+//     console.log("password salah");
+//     }
+// }else{
+//     console.log("email belum terdaftar");
+// }
+
+
+
+// perbedaan else dan else if
+// kalo else if artinya masih ada di bawahnya / kecuali jika
+// klau else artinya tdk ada mi di bawahnya
+
+
+
+// switchcase
+
+// const warna = 'hitam';
+
+// switch (warna) {
+//     case "hitam":
+//         console.log("warna Hitam ");
+//         break;
+//     case "putih":
+//         console.log("warna Putih");
+//     break;
+//     default:
+//         console.log("tidak ada warna pilihan");
+//         break;
+// }
+
+
+
+var pendapatan = 300000;
+var komisi, jasa, total;
+
+if (pendapatan >= 0 && pendapatan <= 200000){
+    jasa = 10000
+    komisi = 0.1* pendapatan;
+}
+else if (pendapatan <= 500000)
+{
+    jasa = 200000;
+    komisi = 0.15* pendapatan;
+}
+else
+{
+    jasa = 300000;
+    komisi = 0.2;
+}
+{
+    total = komisi + jasa;
+    console.log(jasa);
+    console.log(komisi);
+    console.log(total);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
